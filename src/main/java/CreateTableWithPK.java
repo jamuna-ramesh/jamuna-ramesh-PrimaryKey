b635d5af94835b78ba555de1fd84e92279faad11
jamuna-ramesh <jamuna26062002@gmail.com>
2025-04-26 19:22:07 +0530

    Done

--- src/main/java/CreateTableWithPK.java
+++ src/main/java/CreateTableWithPK.java
@@ -1,9 +1,9 @@
 
 import Util.ConnectionUtil;
-import Util.FileUtil;
+//import Util.FileUtil;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
+//import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -55,7 +55,8 @@ public class CreateTableWithPK {
          *
          * NOTE: The "id" column is what we are going to define as the primary key.
          */
-        String sql = FileUtil.parseSQLFile("problem1.sql");
+        //String sql = FileUtil.parseSQLFile("problem1.sql");
+        String sql = "CREATE TABLE song(id SERIAL PRIMARY KEY,title varchar(100),artist varchar(100))";
 
         try {
             Connection connection = ConnectionUtil.getConnection();
